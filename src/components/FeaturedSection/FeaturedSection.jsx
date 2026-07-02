8273e98ec5ace2338c1d4ec93298f12a66638460
Ian Cortes <iancscortes@gmail.com>
2026-07-02 20:41:02 -0300

    Cria estrutura da seção de matérias em destaque

---
+++ src/components/FeaturedSection/FeaturedSection.jsx
@@ -0,0 +1,22 @@
+import "./FeaturedSection.css";
+
+function FeaturedSection() {
+    return(
+
+        <section className="featured-section">
+
+            <div className="featured-left">
+
+            </div>
+
+            <div className="featured-right">
+
+            </div>
+
+        </section>
+
+    );
+
+}
+
+export default FeaturedSection;
